76097261ca601d0a1313aa1887fcc3d52398ec84
Allison Payne <allison@apayneinthe.app>
2023-06-09 03:10:36 -0400

    Cleans up defaults on base conditions. Fixes IsEquippedHasKeyword. (#1)

--- OARStructures/Conditions/Condition.cs
+++ OARStructures/Conditions/Condition.cs
@@ -1,11 +1,11 @@
-﻿using System.Text.Json.Serialization;
-
-namespace DARtoOAR.OARStructures.Conditions
+﻿namespace DARtoOAR.OARStructures.Conditions
 {
     internal class Condition
     {
+        private const string CONFIG_FILE_DEFAULT_VERSION = "1.0.0.0";
         public string? condition { get; set; }
-        public string? requiredVersion { get; set; }
-        public bool? negated { get; set; }
+        public string requiredVersion { get; set; } = CONFIG_FILE_DEFAULT_VERSION;
+        public bool negated { get; set; } = false;
+        public bool keepRandomResultsOnLoop { get; set; } = true;
     }
 }
